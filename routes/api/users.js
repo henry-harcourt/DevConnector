96e4a5c81a5e22ef2b61e03cc70e602d2db22fef
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-04-07 12:24:38 +1200

    add register user post route

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,29 @@
 const express = require('express')
 const router = express.Router()
+const { check, validationResult } = require('express-validator/check')
 
-// @route GET   api/users
-// @description Test route
+// @route POST   api/users
+// @description register user
 // @access      Public (no auth token required)
-router.get('/', (req, res) => res.send('User route'))
+router.post('/', [
+    
+    // check test implemented through express-validator/check npm package
+    check('name', 'Name is required')
+        .not()
+        .isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+],
+    (req, res) => {
+
+        // here, if the checks above are not met, the server will see if the error message is not empty and will respond with
+        // a 404 and an array of the errors. 
+        const errors = validationResult(req)
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() })
+        }
+
+        res.send('User route')
+    })
 
 module.exports = router
