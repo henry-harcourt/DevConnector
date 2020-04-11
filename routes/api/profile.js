640bd0eae9b2447d8c3ef6a7d934f026e71c827a
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-04-11 12:43:45 +1200

    add route to add experience to profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -136,7 +136,7 @@ router.get('/user/:user_id', async (req, res) => {
         res.json(profile)
     } catch (err) {
         console.error(err.message)
-        if (err.kind == 'ObjectId'){
+        if (err.kind == 'ObjectId') {
             return res.status(400).json({ msg: 'Profile not found' })
         }
         res.status(500).send('Server Error')
@@ -153,11 +153,65 @@ router.delete('/', auth, async (req, res) => {
 
         // remove profile
         await Profile.findOneAndRemove({ user: req.user.id })
-        
+
         // remove user
         await User.findOneAndRemove({ _id: req.user.id })
 
         res.json({ msg: 'User deleted' })
+    } catch (err) {
+        console.error(err.message)
+        res.status(500).send('Server Error')
+    }
+})
+
+// @route       PUT api/profile/experience
+// @description  Add profile experience
+// @access      Private
+
+router.put('/experience', [auth, [
+    check('title', 'Title is required')
+        .not()
+        .isEmpty(),
+    check('company', 'Company is required')
+        .not()
+        .isEmpty(),
+    check('from', 'From date is required')
+        .not()
+        .isEmpty(),
+]], async (req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() })
+    }
+
+    const {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    } = req.body
+
+    // creates an objec with the data thatt the user submits
+    const NewExp = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description
+    }
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id })
+
+        profile.experience.unshift(NewExp) // unshfit method is same as .push except it sends new data to the front of the array
+        await profile.save()
+
+        res.json(profile)
     } catch (err) {
         console.error(err.message)
         res.status(500).send('Server Error')
