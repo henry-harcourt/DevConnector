9a1e5c1752d9624ee031855d1d5f3b4e92c5f4d5
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-04-13 12:57:25 +1200

    add Post model

---
+++ models/Posts.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+
+// connects post to a user. references user model
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'users'
+    },
+    text: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String
+    },
+    avatar: {
+        type: String
+    },
+    likes: [
+        {
+            user: {
+                type: Schema.Types.ObjectId, // single user can only like posts once. includes array of user object IDs 
+                ref: 'users'
+            }
+        }
+    ],
+    comments: [
+        {
+            user: {
+                type: Schema.Types.ObjectId,
+                ref: 'users'
+            },
+            text: {
+                type: String,
+                required: true
+            },
+            name: {
+                type: String
+            },
+            avatar: {
+                type: String
+            },
+            date: {
+                type: Date,
+                default: Date.now
+            }
+        }
+    ],
+    date: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Post = mongoose.model('post', PostSchema)
