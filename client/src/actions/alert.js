8ccda6e910e6bf8c2e05763809039d1089facb83
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-04-19 13:14:54 +1200

    set up alert system with redux

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -1,11 +1,13 @@
-import uuid from 'uuid'
+import {v4 as uuidv4 } from 'uuid'
 import { SET_ALERT, REMOVE_ALERT } from './types'
 
 // thunk allows the double arrrow function
-export  const setAlert = (msg, alertType) => dispatch => {
-    const id = uuid.v4() // gives us a random long string
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+    const id = uuidv4() // gives us a random long string
     dispatch({
         type: SET_ALERT,
         payload: { msg, alertType, id }
     })
+
+    setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout)
 }
