7a432d26aa35d30891c7a1d1a8e36561624f3196
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-04-22 11:58:37 +1200

    add dashboard markup and loading gif

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,31 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Fragment } from 'react'
+import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getCurrentProfile } from '../../actions/profile'
+import Spinner from '../layouts/Spinner'
 
 
 
-const Dashboard = ({ getCurrentProfile, auth, profile }) => {
+const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
         getCurrentProfile()
     }, [])
-    
-    return (
-        <div>
-            Dashboard
-        </div>
-    )
+
+    return loading && profile === null ? (
+        <Spinner />
+    ) : (
+            <Fragment>
+                <h1 className="large text-primary">Dashboard</h1>
+                <p className="lead">
+                    <i className="fas fa-user" /> Welcome {user && user.name}
+                </p>
+                {profile !== null ? (
+                    <Fragment>has</Fragment>
+        ) : (
+                    <Fragment>
+                        <p>You haven't set up a profile yet. Would you like to add some info?</p>
+                        <Link to='/create-profile' className='btn btn-primary my-1'>
+                            Create Profile
+                        </Link>
+                    </Fragment>
+            )}
+            </Fragment>
+                )
 }
 
 Dashboard.propTypes = {
-    getCurrentProfile: PropTypes.func.isRequired,
+                    getCurrentProfile: PropTypes.func.isRequired,
     auth: PropTypes.object.isRequired,
     profile: PropTypes.object.isRequired
 }
 
 const mapStateToProps = state => ({
-    auth: state.auth,
+                    auth: state.auth,
     profile: state.profile
 })
 
-export default connect(mapStateToProps, { getCurrentProfile })(Dashboard)
+export default connect(mapStateToProps, {getCurrentProfile})(Dashboard)
