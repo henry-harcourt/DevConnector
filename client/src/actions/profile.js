8762d860ffb5433868c7713d4090f1715b8b221c
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-04-24 12:53:42 +1200

    add experince and add education forms

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -3,6 +3,7 @@ import { setAlert } from './alert'
 
 import {
     GET_PROFILE,
+    UPDATE_PROFILE,
     PROFILE_ERROR
 } from './types'
 // import { set } from 'mongoose'
@@ -44,6 +45,80 @@ export const createProfile = (formData, history, edit = false) => async dispatch
         })
 
         dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'))
+
+        if(!edit) {
+            history.push('/dashboard')
+        }
+    } catch (err) {
+
+        const errors = err.response.data.errors
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    } 
+}
+
+// Add experience 
+
+export const addExperience = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.put('/api/profile/experience', formData, config)
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert('Experience added', 'Profile Updated', 'success'))
+
+        if(!edit) {
+            history.push('/dashboard')
+        }
+    } catch (err) {
+
+        const errors = err.response.data.errors
+
+        if (errors) {
+            errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+        }
+
+        dispatch({
+            type: PROFILE_ERROR,
+            payload: { msg: err.response.statusText, status: err.response.status }
+        })
+    } 
+}
+
+// Add education 
+
+export const addEducation = (formData, history, edit = false) => async dispatch => {
+    try {
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const res = await axios.put('/api/profile/education', formData, config)
+
+        dispatch({
+            type: UPDATE_PROFILE,
+            payload: res.data
+        })
+
+        dispatch(setAlert('Education added', 'Profile Updated', 'success'))
 
         if(!edit) {
             history.push('/dashboard')
