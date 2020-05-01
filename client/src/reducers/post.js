ad01378d9e66da73c4c8047480bee13ed01f26b0
Henry-Harcourt <hnry.harcourt@gmail.com>
2020-05-01 16:36:01 +1200

    add update like functionality

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,7 @@
 import {
     GET_POSTS,
-    POST_ERROR
+    POST_ERROR,
+    UPDATE_LIKE
 } from '../actions/types'
 
 const initialState = {
@@ -24,6 +25,15 @@ export default function (state = initialState, action) {
             return {
                 ...state,
                 error: payload,
+                loading: false
+            }
+        case UPDATE_LIKE:
+            return {
+                ...state,
+                // line below checks it is the corrrect post that we are adding or removing a like to/from. 
+                // if its a match then - return the post as is, then - return the post with ammended likes.
+                // final condition: if the id doesnt match - retun the post and forget any like changes.  
+                posts: state.posts.map(post => post._id === payload.id ? { ...post, likes: payload.likes } : post),
                 loading: false
             }
         default:
